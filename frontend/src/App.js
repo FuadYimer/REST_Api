03510efedd0d4de953c3d622cfe8252cfe6fd700
FuadYimer <36766534+FuadYimer@users.noreply.github.com>
2023-01-19 16:21:07 +0300

    frontend added with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import { ToastContainer } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -21,7 +21,7 @@ function App() {
           </Routes>
         </div>
       </Router>
-      {/* <ToastContainer /> */}
+      <ToastContainer />
     </>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import { useState, useEffect } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
-// import { toast } from 'react-toastify'
-import { FaSignInAlt, FaUser } from 'react-icons/fa'
-// import { register, reset } from '../features/auth/authSlice'
-// import Spinner from '../components/Spinner'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -16,38 +16,46 @@ function Login() {
 
   const { email, password } = formData
 
-//   const navigate = useNavigate()
-//   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-//   const { user, isLoading, isError, isSuccess, message } = useSelector(
-//     (state) => state.auth
-//   )
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
-//   useEffect(() => {
-//     if (isError) {
-//       toast.error(message)
-//     }
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
 
-//     if (isSuccess || user) {
-//       navigate('/')
-//     }
+    if (isSuccess || user) {
+      navigate('/')
+    }
 
-//     dispatch(reset())
-//   }, [user, isError, isSuccess, message, navigate, dispatch])
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value
+      ...prevState,
+      [e.target.name]: e.target.value,
     }))
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
-   
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
   }
 
- 
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -55,12 +63,11 @@ function Login() {
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Login and Set Goals</p>
+        <p>Login and start setting goals</p>
       </section>
 
       <section className='form'>
         <form onSubmit={onSubmit}>
-         
           <div className='form-group'>
             <input
               type='email'
@@ -83,7 +90,7 @@ function Login() {
               onChange={onChange}
             />
           </div>
-          
+
           <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
